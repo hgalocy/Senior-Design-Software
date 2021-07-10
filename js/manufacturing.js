3585ae73659f17f58178ade17e56bcc8470bdd78
hgalocy <hannah@galocy.com>
2021-07-10 15:30:05 -0700

    first commit - html and css 50% done

---
+++ js/manufacturing.js
@@ -0,0 +1,7 @@
+const manufStartTestsBtn = document.getElementById('manufStartTestsBtn')
+manufStartTestsBtn.addEventListener('click', function(){
+    console.log(window.innerHeight)
+    console.log(window.outerHeight)
+    console.log(window.innerWidth)
+    console.log(window.outerWidth)
+});
---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1050,
+        height: 605,
+        icon:'guitar.png'
+    });
+    win.removeMenu();
+    win.loadFile('html/manufacturing.html');
+    //win.webContents.openDevTools(); //uncomment for debugging
+}
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
